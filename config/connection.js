a9da0cb3865118c751843751440d9ad8e32ae0e6
Jay A <jautus@hotmail.com>
2022-07-30 09:58:53 -0700

    added connection and server js content

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const { connect, connection } = require('mongoose');
+
+// After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
+// Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
+const connectionString =
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetworkDB';
+
+connect(connectionString, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Use this to log mongo queries being executed!
+set('debug', true);
+
+module.exports = connection;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const cwd = process.cwd();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
+const activity = cwd.includes('01-Activities')
+    ? cwd.split('/01-Activities/')[1]
+    : cwd;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+app.use(express.static('public'));
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`🌍 API server for ${activity} running on port ${PORT}!`);
+    });
+});
